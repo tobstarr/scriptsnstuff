e30f7865d0c48a96cd8dc068cba8229e95e1c09c
Tobias Schwab <tobias.schwab@dynport.de>
2015-07-12 13:21:19 +0200

    install recommended packages for rbenv setup

--- setup_rbenv.sh
+++ setup_rbenv.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -xe
 
+sudo apt-get install --no-install-recommends -y autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm3 libgdbm-dev
+
 if [[ ! -e $HOME/.rbenv ]]; then
   git clone https://github.com/sstephenson/rbenv.git $HOME/.rbenv
 fi
