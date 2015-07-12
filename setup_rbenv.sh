f533dd916376ebb95c98543ffeccfe9cd3ccf7e6
Tobias Schwab <tobias.schwab@dynport.de>
2015-07-12 12:46:15 +0200

    document how to add rbenv to shell

--- setup_rbenv.sh
+++ setup_rbenv.sh
@@ -8,3 +8,10 @@ fi
 if [[ ! -e $HOME/.rbenv/plugins/ruby-build ]]; then
   git clone https://github.com/sstephenson/ruby-build.git $HOME/.rbenv/plugins/ruby-build
 fi
+
+
+# add this to your .profile
+# if [[ -e ${HOME}/.rbenv ]]; then
+#   export PATH=${HOME}/.rbenv/shims:${PATH}:${HOME}/.rbenv/bin
+#   eval "$(rbenv init -)"
+# fi
